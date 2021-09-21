6c9d63f8a88324acad79fafa5cd704b2f96a40a7
João Lucas Mota <joaomota59@hotmail.com>
2021-09-21 20:17:32 -0300

    finalizando adaline - Treinamento

--- adaline.py
+++ adaline.py
@@ -8,7 +8,7 @@ def degrau(u):
 if  __name__ == '__main__':
     quantAmostras = int(input("Quantidade de amostras que possui o conjunto de treinamento -> "))
     precisaoRequerida = float(input("Precisão Requerida(ε) -> "))
-    numMaxEpocas = float(input("Número máximo de épocas -> "))
+    numMaxEpocas = int(input("Número máximo de épocas -> "))
     amostras = []
 
     for i in range(quantAmostras):
@@ -29,32 +29,77 @@ if  __name__ == '__main__':
 
     epoca = 0
 
-    vetPotAtv = []
-    for indiceAmostra in range(quantAmostras):
-        entradas = [-1] + amostras[indiceAmostra][:-1]#wo = -1 no perceptron ...trocar dps o 0 aqui
+    while True:
+        vetPotAtv = []
+        print("\nÉpoca:",epoca+1)
+        for indiceAmostra in range(quantAmostras):
+            entradas = [-1] + amostras[indiceAmostra][:-1]#wo = -1 no perceptron ...trocar dps o 0 aqui
 
-        u = 0
+            u = 0
+            print("u = ",end="")
+            for i in range(len(entradas)):
+                u+= entradas[i]*w[i]
+                if(i!=len(entradas) - 1):
+                    print(entradas[i],"*",w[i],"+",end="")
+                    continue
+                print(entradas[i],"*",w[i],"=",u,end="\n")
 
-        #print("Época:",epoca+1)
-        print("u = ",end="")
-        for i in range(len(entradas)):
-            u+= entradas[i]*w[i]
-            if(i!=len(entradas) - 1):
-                print(entradas[i],"*",w[i],"+",end="")
-                continue
-            print(entradas[i],"*",w[i],"=",u,end="\n")
+            #epoca += 1
+            
+            vetPotAtv.append(u)
+        print("W =",w)
+        print("Eqm_Anterior(w) = ",end="")
 
-        #epoca += 1
+        equacao = "1/"+str(len(vetPotAtv))+"*("
         
-        vetPotAtv.append(u)
+        for index,potencial in enumerate(vetPotAtv):#Eqm Anterior
+            if(index != len(vetPotAtv) - 1):
+                equacao += "("+str(amostras[index][-1])+"-"+str(potencial)+")**2 + "
+                continue
+            equacao += "("+str(amostras[index][-1])+"-"+str(potencial)+")**2 )"
+        print(equacao,"=",eval(equacao))
 
-    print("E(w) = ",end="")
+        vetPotAtv = []
 
-    equacao = "1/2*("
+        for indiceAmostra in range(quantAmostras):
+            u = 0
+
+            #print("Época:",epoca+1)
+            print("u = ",end="")
+            for i in range(len(entradas)):
+                u+= entradas[i]*w[i]
+                if(i!=len(entradas) - 1):
+                    print(entradas[i],"*",w[i],"+",end="")
+                    continue
+                print(entradas[i],"*",w[i],"=",u,end="\n")
+
+            for i in range(len(w)):
+                w[i] = w[i] + taxaDeAprendizagem*(amostras[indiceAmostra][-1]-u)*entradas[i]
+
+            vetPotAtv.append(u)
+
+
+        print("Eqm_Atual(w) = ",end="")
+
+        equacao2 = "1/"+str(len(vetPotAtv))+"*("
+        for index,potencial in enumerate(vetPotAtv):#Eqm Atual
+            if(index != len(vetPotAtv) - 1):
+                equacao2 += "("+str(amostras[index][-1])+"-"+str(potencial)+")**2 + "
+                continue
+            equacao2 += "("+str(amostras[index][-1])+"-"+str(potencial)+")**2 )"
+        print(equacao2,"=",eval(equacao2))
+        print("W =",w)
+
+        epoca+=1
+
+        diferencaAtualeAnterior = abs(eval(equacao) - eval(equacao2))
+
+        print("|Eqm_Atual(w) - Eqm_Anterior(w)| =",diferencaAtualeAnterior)
+
+        if diferencaAtualeAnterior <= precisaoRequerida or numMaxEpocas == epoca:
+            break
     
-    for index,potencial in enumerate(vetPotAtv):
-        if(index != len(vetPotAtv) - 1):
-            equacao += "("+str(amostras[index][-1])+"-"+str(potencial)+")**2 + "
-            continue
-        equacao += "("+str(amostras[index][-1])+"-"+str(potencial)+")**2 )"
-    print(equacao)
+    
+
+
+        
