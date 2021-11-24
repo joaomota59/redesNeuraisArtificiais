e2af1799bc0c06bb197f1e94f22dd1ae9d63de0f
João Lucas Mota <joaomota59@hotmail.com>
2021-11-24 16:07:29 -0300

    add float

--- convolucao-stride-padding-kernel.py
+++ convolucao-stride-padding-kernel.py
@@ -2,23 +2,23 @@ from scipy import signal
 import numpy as np
 
 def convolucao (entrada,filtro,stride=1,modo="valid"):
-    filtro = np.array(filtro,np.uint8)
-    entrada = np.array(entrada,np.uint8)
+    filtro = np.array(filtro,np.float32)
+    entrada = np.array(entrada,np.float32)
 
     saida = signal.convolve2d(entrada, filtro[::-1, ::-1], mode=modo)[::stride, ::stride]
     print(saida)
 
 
 if __name__ == "__main__":
-    filtro = [[1,0,1],
-          [1,0,0],
-          [0,0,2]]
+    filtro = [[0.1,0.2,0.6],
+          [0.6,0.8,0.7],
+          [0.5,0.4,0.3]]
     entrada = [
-                [6,3,4,4,5,0,3],
-                [4,7,4,0,4,0,4],
-                [7,0,2,3,4,5,2],
-                [0,0,1,1,0,0,0],
-                [0,1,1,0,0,0,0]
+                [0.0,0.0,0.0,0.0,0.0],
+                [0.0,0.2,0.4,0.3,0.0],
+                [0.0,0.3,0.9,0.6,0.0],
+                [0.0,0.9,0.1,0.2,0.0],
+                [0.0,0.0,0.0,0.0,0.0]
               ]
     stride = 1
     modo = "valid"
